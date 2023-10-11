90b54203c08656492c5aeee577d6888483b5fd89
francineee09 <francine_hallar@dlsu.edu.ph>
2023-10-11 14:32:29 +0800

    v.2023.10.11.14.31
    - Added reviews.html
    - Edited dashboard.html chatbox

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,8 @@
-//script.js
-//sign in
+// Sign up
 document.addEventListener('DOMContentLoaded', function () {
     const signupForm = document.getElementById('signup-form');
 
-    signupForm.addEventListener('submit', function (event) {
+    signupForm.addEventListener('submit', async function (event) {
         event.preventDefault(); // Prevent the form from submitting by default
 
         // Get form input values
@@ -18,25 +17,55 @@ document.addEventListener('DOMContentLoaded', function () {
             return; // Prevent form submission if passwords don't match
         }
 
-        // You can add more validation checks here for email format, password strength, etc.
+        // Email Format Validation
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            alert('Please enter a valid email address.');
+            return;
+        }
 
-        // If client-side validation passes, you can send the data to the server for further processing
+        // Password Strength Validation
+        if (password.length < 8 || !/[A-Za-z]/.test(password) || !/\d/.test(password)) {
+            alert('Password must be at least 8 characters long and contain both letters and numbers.');
+            return;
+        }
+
+        // If client-side validation passes, send the data to the server for registration
         const userData = {
             username: username,
             email: email,
             password: password,
         };
 
-        // Simulate sending data to the server (replace this with actual server interaction)
-        console.log('User data to be sent to the server:', userData);
+        // Send the user data to the server using a fetch or AJAX request
+        try {
+            const response = await fetch('/api/signup', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(userData),
+            });
 
-        // Clear the form or redirect to a success page
-        signupForm.reset(); // Clear the form fields
+            if (response.ok) {
+                // Registration successful, you can redirect to a login page or display a success message
+                alert('Registration successful! Please log in.');
+                signupForm.reset(); // Clear the form fields
+            } else {
+                // Registration failed, handle errors (e.g., duplicate username or email)
+                const errorData = await response.json();
+                alert(`Registration failed: ${errorData.message}`);
+            }
+        } catch (error) {
+            console.error('Error:', error);
+            alert('An error occurred while processing your request. Please try again later.');
+        }
     });
 });
 //up until here
 
-//login js
+
+//Login
 document.addEventListener('DOMContentLoaded', function () {
     const loginForm = document.getElementById('login-form');
 
@@ -67,6 +96,8 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 //up until here
+
+
 
 // Function to handle post submission
 function submitPost(event) {
@@ -123,6 +154,134 @@ function submitPost(event) {
 const postForm = document.getElementById("postForm");
 postForm.addEventListener("submit", submitPost);
 
+$(document).ready(function() {
+    // Enable Bootstrap collapse
+    $('[data-toggle="collapse"]').collapse();
+
+    // Handle click events on the "Messages" link to toggle the panel
+    $('.nav-link[data-toggle="collapse"]').on('click', function() {
+        // Close all other open panels when clicking on this link
+        $('.nav-link[data-toggle="collapse"]').not(this).each(function() {
+            $($(this).data('target')).collapse('hide');
+        });
+    });
+});
+//up until here
+
+//MESSAGES
+document.addEventListener("DOMContentLoaded", function () {
+    const chatIcon = document.getElementById("chatIcon");
+    const chatBox = document.getElementById("chatBox");
+    const closeChat = document.getElementById("closeChat");
+    const messageInput = document.getElementById("messageInput");
+    const chatMessages = document.getElementById("chatMessages");
+    const sendMessageButton = document.getElementById("sendMessage");
+    const chatEntries = document.querySelectorAll(".chat-entry");
+
+    // Function to load chat data from local storage
+    function loadChatData() {
+        const chatDataString = localStorage.getItem("chatData");
+        return chatDataString ? JSON.parse(chatDataString) : {};
+    }
+
+    // Function to save chat data to local storage
+    function saveChatData(chatData) {
+        localStorage.setItem("chatData", JSON.stringify(chatData));
+    }
+
+    // Create an object to store chat messages for each chat entry
+    let chatData = loadChatData();
+
+    // Initialize the active chat entry
+    let activeChatEntry = null;
+
+    // Function to toggle the chat box visibility
+    function toggleChat() {
+        chatBox.style.display = chatBox.style.display === "none" ? "block" : "none";
+    }
+
+    // Close the chat box by default when the page loads
+    chatBox.style.display = "none";
+
+    // Event listener to toggle the chat box
+    chatIcon.addEventListener("click", toggleChat);
+    closeChat.addEventListener("click", toggleChat);
+
+    // Function to add a message to the chat
+    function addMessage(messageText, sender) {
+        const messageElement = document.createElement("div");
+        messageElement.classList.add("message", sender);
+        messageElement.innerText = messageText;
+        chatMessages.appendChild(messageElement);
+
+        // Scroll to the bottom after adding a new message
+        scrollToBottom();
+    }
+
+    messageInput.addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            sendMessageButton.click();
+        }
+    });
+
+    // Function to clear chat messages
+    function clearChatMessages() {
+        chatMessages.innerHTML = "";
+    }
+
+    // Event listener to send a message
+    sendMessageButton.addEventListener("click", function () {
+        const messageText = messageInput.value;
+        if (messageText.trim() !== "") {
+            addMessage(messageText, "user");
+            messageInput.value = "";
+
+            // If there is an active chat entry, add the message to its chatData
+            if (activeChatEntry) {
+                const chatName = activeChatEntry.querySelector("span").innerText;
+                if (!chatData[chatName]) {
+                    chatData[chatName] = [];
+                }
+                chatData[chatName].push({ text: messageText, sender: "user" });
+                saveChatData(chatData);
+            }
+        }
+    });
+
+    // Event listener to handle chat entry click
+    chatEntries.forEach(function (entry) {
+        entry.addEventListener("click", function () {
+            selectChatEntry(entry);
+            clearChatMessages();
+            const chatName = entry.querySelector("span").innerText;
+            const chatMessagesData = chatData[chatName];
+            if (chatMessagesData) {
+                chatMessagesData.forEach(function (messageData) {
+                    addMessage(messageData.text, messageData.sender);
+                });
+            }
+        });
+    });
+
+    // Function to select a chat entry
+    function selectChatEntry(entry) {
+        chatEntries.forEach(function (chatEntry) {
+            chatEntry.classList.remove("active");
+        });
+        entry.classList.add("active");
+        activeChatEntry = entry;
+    }
+
+    // Function to scroll to the bottom of the chat messages
+    function scrollToBottom() {
+        chatMessages.scrollTop = chatMessages.scrollHeight;
+    }
+
+    // Example: Adding some initial messages
+    chatEntries[0].click(); // Simulate clicking on the first chat entry to display its messages
+});
+
 //game-library
 // game-library.js
 
@@ -162,5 +321,3 @@ document.getElementById('addGameForm').addEventListener('submit', (event) => {
 
 // Initial display of the game library
 displayLibrary();
-
-
