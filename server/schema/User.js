a7a4dd0f6c22e4df0dc5fa65fc1389cf67ba276c
Josh-Dane <97420287+Josh-Dane@users.noreply.github.com>
2023-11-15 22:19:13 +0800

    Update User.js

--- server/schema/User.js
+++ server/schema/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true }
-  // Add other fields as needed for your application
+  // Add other fields if needed for further improvements. 
 });
 
 module.exports = mongoose.model('User', userSchema);
