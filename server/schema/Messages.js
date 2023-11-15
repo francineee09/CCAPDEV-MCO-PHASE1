6eaf1c06a5f9233dc46e700df827d002de0e1962
Josh-Dane <97420287+Josh-Dane@users.noreply.github.com>
2023-11-15 22:17:25 +0800

    Update Messages.js

--- server/schema/Messages.js
+++ server/schema/Messages.js
@@ -3,12 +3,12 @@
 const mongoose = require('mongoose');
 
 const messageSchema = new mongoose.Schema({
-  sender: String, // You may later change this to reference a User model
+  sender: String, // will later change this to reference a User model
   content: {
     type: String,
     required: true,
   },
-  chat: String, // You may later change this to reference a Chat model or use chat IDs
+  chat: String, // will later change this to reference a Chat model or use chat IDs
   timestamp: {
     type: Date,
     default: Date.now,
